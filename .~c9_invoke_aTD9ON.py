89a5b6be255c60cc685828c442f606031192cf8e
ceciliaB97 <cecilia.belon@hotmail.com>
2021-05-13 23:40:44 -0300

    Added files
    Added files

---
+++ .~c9_invoke_aTD9ON.py
@@ -0,0 +1,37 @@
+import random
+
+from flask import Flask, redirect, render_template, request, session
+from flask_session import Session
+
+app = Flask(__name__)
+app.config["SESSIONS_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+db = SQL('sqlite:///tasks.db')
+
+@app.route("/")
+def tasks():
+    if "todos" not in session:
+        session["todos"] = []
+    return render_template("etasks.html", todos=session["todos"])
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "GET":
+        return render_template("add.html")
+    else:
+        todo = request.form.get("task")
+        session["todos"].append(todo)
+        return redirect("/")
+
+@app.route("/delete", methods=["GET", "POST"])
+def delete():
+    if request.method == "GET":
+        if "todos" not in session:
+            session["todos"] = []
+        return render_template("delete.html", todos=session["todos"])
+    else:
+        todo = request.form.get("{{ todo }}")
+        session["todos"].remove(todo)
+        return redirect("/")
---
+++ README.md
@@ -0,0 +1,8 @@
+A website application where students, and everybody can upload their tasks
+
+The options are
+    View the tasks
+    Insert a task
+    Delete a task
+
+In case there are no tasks a message is displayed to the user
